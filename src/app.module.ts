a6bb1389b010b63f272fd3457f9d79d76699bc86
Marcelo Alvarez <rmalvarezq@utn.edu.ec>
2023-05-28 08:06:09 -0500

    typeORM added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/user.module';
 import { ProductsModule } from './products/product.module';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [CatsModule, UsersModule,ProductsModule],
+  imports: [
+    CatsModule,
+    UsersModule,
+    ProductsModule,
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
